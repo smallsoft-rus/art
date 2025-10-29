bf8d48dac2c1ce58c006c07d14454dd79605e396
SmallSoft <vadim--111@yandex.ru>
2025-10-29 22:28:01 +0500

    Add content

--- build-html.cmd
+++ build-html.cmd
@@ -3,6 +3,14 @@ rem Generate HTML from XML data
 set PATH=C:\Distr\CatalogTools;%PATH%
 cd vitalina
 CatalogTools --html --homelink vitalina.xml
+cd ..\arnaut
+CatalogTools --html --homelink catalog.xml
 cd ..\asya.less
 CatalogTools --html --homelink --layout Indexed asyaless.xml
+cd ..\filkins
+CatalogTools --html --homelink --layout Indexed catalog.xml
+cd ..\gozamnoi
+CatalogTools --html --homelink catalog.xml
+cd ..\rinasun
+CatalogTools --html --homelink --layout Indexed rinasun.xml
 PAUSE
